dab2848540213b64396176b337f595be4f8a0b14
NgoziEmeh <zingo1ng@yahoo.com>
2021-10-05 04:51:03 -0700

    tip amount

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ print("Welcome to Ngozi's Tip Calculator: The Python Version")
 total_bill = int(input("What was the total bill?"))
 
 tip_percentage = int(input("What percentage of  tip will you like to give?"))
+tip_amount = total_bill + (total_bill * (tip_percentage/100))
