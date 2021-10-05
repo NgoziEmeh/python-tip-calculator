d2c48195cfa17b4cf8406c4d3ca3142a648e98d6
NgoziEmeh <zingo1ng@yahoo.com>
2021-10-05 05:03:19 -0700

    comments

--- main.py
+++ main.py
@@ -2,7 +2,14 @@
 print("Welcome to Ngozi's Tip Calculator: The Python Version")
 # Total amount of the bill
 total_bill = int(input("What was the total bill?"))
+
 # Percentage to be given as tip
-tip_percentage = int(input("What percentage of  tip will you like to give?"))
+tip_percentage = int(input("What percentage of tip will you like to give?"))
+
+# Amount to be paid. Tip + Total bill
 tip_amount = total_bill + (total_bill * (tip_percentage/100))
+
+# Number of people to share the bill
 spliters = int(input("How many people to slit the bill?"))
+tip_per_person = tip_amount / spliters
+print(tip_per_person)
